77d3a97f1d01207941caa7ac73326467eb81b6f9
Masoud A <amini141@gmail.com>
2014-03-03 11:07:01 +0330

    Update rsmembershipzarinpal.php

--- Zarinpal_rsmembership/rsmembershipzarinpal.php
+++ Zarinpal_rsmembership/rsmembershipzarinpal.php
@@ -41,7 +41,7 @@ class plgSystemRSMembershipZarinpal extends JPlugin
 	{
 		if (!$this->canRun()) return;
 		if ($plugin != $this->_plugin->name) return false;
-		$desc = $transaction->id ;
+	$desc = 'پرداخت مربوط به کاربر : ' . $transaction->user_email;
     	$api = $this->_params->get('api');
         $amount = $transaction->price;
 
